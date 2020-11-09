0a5d72c956565cd5140bc5edeb849e9e070f2048
pavellevanets-gc <pavel.levanets@itechart-group.com>
2020-11-09 16:09:46 +0300

    commit

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,7 @@
 exports.config = {
+    // Browserstack Config
+    user: process.env.BROWSERSTACK_USERNAME,
+    key: process.env.BROWSERSTACK_KEY,
     //
     // ====================
     // Runner Configuration
@@ -106,7 +109,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: ['browserstack'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -128,7 +131,17 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['spec'],
+    reporters: [['allure', {
+        outputDir: 'allure-results',
+
+    },
+],
+['junit', {
+    outputDir: './report'
+}]
+],
+    
+    
 
 
     
